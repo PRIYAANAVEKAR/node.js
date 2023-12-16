2d933207e4b7e0e1fb323d0cedf74bf70163823b
PRIYAANAVEKAR <priyanavekar@gmail.com>
2023-12-16 21:31:11 +0530

    my first react.js

---
+++ vite-project/src/Appcard.jsx
@@ -0,0 +1,11 @@
+import card from './card.jsx'
+function Appcard(){
+    return(
+        <Card/>
+        
+       
+
+    );
+
+}
+export default Appcard
---
+++ vite-project/src/card.jsx
@@ -0,0 +1,13 @@
+
+function card(){
+    return(
+        <div className ="card">
+          <img src="https://unsplash.com/s/photos/bird" alt="profile picture"></img>
+          <h2>Priya Anavekar</h2>
+          <p>I am full Stack developer</p>
+
+        </div>
+
+    )
+}
+export default card
